ad41b8811e1b69a3e1a4b6f9f27b0894d38c403d
Dyah Noorcholisa <dyah@Dyahs.local>
2023-04-09 12:20:11 +0700

    configure project

---
+++ src/screens/home/components/card-item/CardItem.tsx
@@ -0,0 +1,76 @@
+import React, { useMemo } from "react";
+import { View, StyleProp, ViewStyle } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import Icon from "react-native-dynamic-vector-icons";
+import RNBounceable from "@freakycoder/react-native-bounceable";
+/**
+ * ? Local Imports
+ */
+import createStyles from "./CardItem.style";
+import { ICardItem } from "@services/models";
+import Text from "@shared-components/text-wrapper/TextWrapper";
+
+type CustomStyleProp = StyleProp<ViewStyle> | Array<StyleProp<ViewStyle>>;
+
+interface ICardItemProps {
+  style?: CustomStyleProp;
+  data: ICardItem;
+  onPress: () => void;
+}
+
+const CardItem: React.FC<ICardItemProps> = ({ style, data, onPress }) => {
+  const theme = useTheme();
+  const { colors } = theme;
+  const styles = useMemo(() => createStyles(theme), [theme]);
+
+  const { name, description, language, star, fork } = data;
+
+  // eslint-disable-next-line react/no-unstable-nested-components
+  const Header = () => (
+    <>
+      <Text h4 bold color={colors.text}>
+        {name}
+      </Text>
+      <Text h5 color={colors.placeholder} style={styles.descriptionTextStyle}>
+        {description}
+      </Text>
+    </>
+  );
+
+  // eslint-disable-next-line react/no-unstable-nested-components
+  const Language = () => (
+    <View style={styles.languageContainer}>
+      <View style={styles.languageColorStyle} />
+      <Text style={styles.valueTextStyle}>{language}</Text>
+    </View>
+  );
+
+  // eslint-disable-next-line react/no-unstable-nested-components
+  const Star = () => (
+    <View style={styles.starContainer}>
+      <Icon name="star-o" type="FontAwesome" color={colors.text} />
+      <Text style={styles.valueTextStyle}>{star}</Text>
+    </View>
+  );
+
+  // eslint-disable-next-line react/no-unstable-nested-components
+  const Fork = () => (
+    <View style={styles.forkContainer}>
+      <Icon name="code-fork" type="FontAwesome" color={colors.text} />
+      <Text style={styles.valueTextStyle}>{fork}</Text>
+    </View>
+  );
+
+  return (
+    <RNBounceable style={[styles.container, style]} onPress={onPress}>
+      <Header />
+      <View style={styles.contentContainer}>
+        <Language />
+        <Star />
+        <Fork />
+      </View>
+    </RNBounceable>
+  );
+};
+
+export default CardItem;
